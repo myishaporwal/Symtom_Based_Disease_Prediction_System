68d1ab5bcfdd7a15a6cfa6ca70500b4aed8edebf
myishaporwal <myishaporwal@gmail.com>
2026-09-14 01:11:39 +0530

    Clear all files

--- Project Files/db_config.py
+++
@@ -1,7 +0,0 @@
-# db_config.py
-
-# Database connection details
-DB_HOST = "localhost"  # Replace with your database host
-DB_USER = "root"  # Replace with your database username
-DB_PASSWORD = "ktom2005"  # Replace with your database password
-DB_NAME = "health_management_2"  # Replace with your database name
